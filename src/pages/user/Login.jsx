57c615a2275e51a9bff6a14a857f805b76055120
Tomoko-hub <haisaitomoco@gmail.com>
2023-02-24 11:58:18 +0200

    edit login.jsx and register.jsx

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async(event)=> {
+    event.preventDefault()
+  } 
+  try {
+    const response = fetch("http://localhost:5000/user/login", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+    const jsonResponse = response.json()
+    alert(jsonResponse.message)
+  }
+  catch(err){
+    alert("Failed to login..")
+  }
+
+
   return (
-    <div>login</div>
+    <div>
+      <p>login</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={email}
+          onChange={
+            (event)=> {
+              setEmail(event.target.value)
+            }
+          }
+          type="text" name="email" placeholder='Put your email address.' required />
+        <input 
+          value={password}
+          onChange={
+            (event)=> {
+              setPassword(event.target.value)
+            }
+          }
+          type="text" name="password" placeholder='Put your password' required />
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
--- src/pages/user/Register.jsx
+++ src/pages/user/Register.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Register = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit =async(event)=> {
+    event.preventDefault()
+    try{
+      const response = await fetch("http://localhost:5000/user/register", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body:JSON.stringify({
+          name: name,
+          email: email,
+          password: password
+        })
+      })
+      const jsonResponse = await response.json()
+      alert(jsonResponse.message)
+    }
+    catch(err){
+      alert('Failed to register your info..')
+    }
+  }
   return (
     <div>
-      Register
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          value={name} 
+          onChange={
+          (event)=> setName(event.target.value) 
+        }
+          type="text" name="name" placeholder='Write Your Name' required
+         />
+          <input 
+            value={email}
+            onChange={
+              (event)=> setEmail(event.target.value)
+            }
+            type="text" name="email" placeholder='Put Your Email' required />
+          <input 
+            value={password}
+            onChange={
+              (event)=> setPassword(event.target.value)
+            }
+            type="text" 
+            name="password" 
+            placeholder='Write password' required />
+        
+        <button>Register</button>
+      </form>
     </div>
   )
 }
